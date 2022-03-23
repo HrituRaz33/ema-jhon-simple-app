771b941b616534017ed26905e93dfdbddd8ac61a
HrituRaz33 <95464215+HrituRaz33@users.noreply.github.com>
2022-03-23 23:04:55 +0600

    add to cart button added dynamicly

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const {name, img, seller, price, ratings} = props.product;
+    const {product, handleAddToCart} = props;
+    const {name, img, seller, price, ratings} = product;
     return (
         <div className='product'>
             <img src={img} alt="" />
              <div className="product-info">
                <p className='product-name'>{name}</p>
-               <p>Price: {price}</p>
+               <p>Price: ${price}</p>
                <p><small>Seller: {seller}</small></p>
                <p><small>Ratings: {ratings}stars</small></p>
              </div>
-             <button className='btn-cart'>
-                 <p>Add to Cart</p>
+             <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                 <p>Add to Cart <i class="fa-solid fa-cart-plus"></i></p>
              </button>
         </div>
     );
