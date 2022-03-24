a96628c64c5a7ece6c60a2ad8555d8ac5992f6e3
HrituRaz33 <95464215+HrituRaz33@users.noreply.github.com>
2022-03-24 17:17:49 +0600

    order summary calculation done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
-const Card = ({cart}) => {
+const Card = (props) => {
+
+    const {cart} = props;
+    
+    let total = 0;
+    let shipping = 0;
+
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = parseFloat((total * 0.1).toFixed(2));
+
+    const grandTotal = total + shipping + tax;
+
     return (
-        <div>
-            <h1>hay i am card</h1>
+        <div className='cart'>
+            <h1>Order Sumarry</h1>
             <p>Selected item: {cart.length}</p>
+            <p>Total Price: ${total} </p>
+            <p>Total Shipping: {shipping}</p>
+            <p>Tex: {tax}</p>
+            <h5>Grand Total: {grandTotal}</h5>
         </div>
     );
 };
